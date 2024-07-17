b0d462a8d62c107b3620a1f1ea9546900adc1c47
wanghaiming <252127886@qq.com>
2024-07-17 11:40:12 +0800

    无

--- whm-baseline-consumer/whm-baseline-demo-consumer/src/main/java/space/whm/demo/DemoConsumerApplication.java
+++ whm-baseline-consumer/whm-baseline-demo-consumer/src/main/java/space/whm/demo/DemoConsumerApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
-@EnableDubbo
 @SpringBootApplication
+@EnableDubbo
 @ImportResource(value = { "whm-baseline-demo-consumer.xml" })
 public class DemoConsumerApplication {
 
