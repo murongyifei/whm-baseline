748a909f1e66d82702aed9be83d61d8e8a50465d
wanghaiming <252127886@qq.com>
2024-10-11 15:51:26 +0800

    nacos默认端口修改问题调查

--- whm-baseline-provider/whm-baseline-demo-provider/src/main/java/space/whm/demo/DemoProviderApplication.java
+++ whm-baseline-provider/whm-baseline-demo-provider/src/main/java/space/whm/demo/DemoProviderApplication.java
@@ -3,11 +3,13 @@ package space.whm.demo;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @EnableDubbo(scanBasePackages = {"space.whm.demo.service.impl"})
 @ImportResource(value = { "classpath:/whm-baseline-demo-provider.xml" })
+@EnableDiscoveryClient
 public class DemoProviderApplication {
 
     public static void main(String[] args) {
