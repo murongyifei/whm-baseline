f25d2b1cf98eff2077d2f3868c5bc3ec8cccf901
aliyun6677295767 <1092542354@qq.com>
2024-10-11 16:27:34 +0800

    test: 11

---
+++ mybatis-plus-clickhouse/src/main/java/com/lzp/mybatisPlusClickhouse/MybatisPlusClickhouseApplication.java
@@ -0,0 +1,13 @@
+package com.lzp.mybatisPlusClickhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisPlusClickhouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlusClickhouseApplication.class, args);
+    }
+
+}
